ae4cab6a94ddc54efd9d6c80e067279b94443c56
Kristin Saunders <kmsaunders7@gmail.com>
2021-03-10 09:21:46 -0600

    add server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+// require the routes
+
+app.use(require("./routes/api_routes"))
+app.use(require("./routes/html_routes"))
+
+
+
+
+
+
+// confirms the server is listening on designated PORT
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
